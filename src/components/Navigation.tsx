ad85bff6ba197a99a28b6fc12e739a5b9a52ceca
hiwafa <mq.func@gmail.com>
2024-04-14 10:54:24 +0200

    First Commit

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { IoMoon, IoMenu, IoClose } from "react-icons/io5";
+
+export default ()=> {
+
+    const handleCloseMenu = ()=> { 
+        // const menu = document.querySelector("#closeMenu");
+        // menu.style.display = "none";
+    }
+
+    return (
+      <div>
+        Nav
+      </div>
+    );
+  }
