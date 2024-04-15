6f36cc5d071ab207d34d3228b49c98c255f4484e
hiwafa <mq.func@gmail.com>
2024-04-15 14:52:35 +0200

    Add Click event

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import { IoMoon, IoMenu, IoClose } from "react-icons/io5";
 import { IoIosMenu } from "react-icons/io";
@@ -5,13 +7,19 @@ import { IoIosMenu } from "react-icons/io";
 export default ()=> {
 
   
+  const handleMobileMenu = ()=> {
+
+    console.log("Hi dear Mobile Menu clicked");
+
+    return false;
+  }
 
     return (
       <div className="bg-indigo-300 flex justify-between">
        <section>logo</section>
        <nav className="absolute bg-blue-500 w-full h-48 flex flex-col items-center justify-evenly text-white">
-        <a className="absolute right-4 top-4" href="">
-        <IoIosMenu size={25} />
+        <a className="absolute right-4 top-4" onClick={handleMobileMenu}>
+          <IoIosMenu size={25} />
         </a>
         <a href="">Home</a>
         <a href="">About</a>
