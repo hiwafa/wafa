7fafb656e8b787efdee8827c0f460560d324b69b
hiwafa <mq.func@gmail.com>
2024-04-16 15:27:01 +0200

    Added simple profile code

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 export default () => {
 
     return (
-        <div className="">
-            Profile
+        <div className="m-5">
+
+           <section className="float-left mr-3">
+            <Image className="rounded-md" src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
+           </section>
+
+           <div className="mt-5">
+                <p className="font-serif font-bold mb-2">Muhammad Wafa</p>
+                <p className="font-mono font-bold mb-2">Software Developer</p>
+                <p className="font-thin">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam libero ad ratione, nesciunt adipisci temporibus omnis, modi recusandae soluta molestias optio maxime magnam consequuntur odit, corrupti sunt eaque distinctio voluptatem!
+                </p>
+           </div>
+           
         </div>
     );
 }
