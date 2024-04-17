4f626df687ae8c48b3c43c5623c59edd92e41e66
hiwafa <mq.func@gmail.com>
2024-04-17 16:14:18 +0200

    updated profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
+import { MdMarkEmailRead } from "react-icons/md";
+import { FaPhoneSquareAlt } from "react-icons/fa";
 
 export default () => {
 
     return (
         <div className="m-5">
 
-           <section className="float-left mr-3 bg-emerald-400 w-full flex flex-1 justify-center">
-            <Image className="rounded-md" src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
+           <section className="float-left mr-3 bg-emerald-400 w-full flex flex-col items-center sm:w-5/12">
+            <Image className="rounded-md self-center " src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
+            <div className="mt-2">
+                <p className="font-serif font-bold mb-2">Muhammad Wafa</p>
+                <div className="w-full flex items-center gap-4">
+                    <MdMarkEmailRead />
+                    <p className="font-mono font-bold mb-2">muhammadwfa@gmail.com</p>
+                </div>
+                <div className="w-full flex items-center gap-4">
+                    <FaPhoneSquareAlt />
+                    <p className="font-mono font-bold mb-2">0178889999</p>
+                </div>
+            </div>
            </section>
 
            <div className="mt-5">
-                <p className="font-serif font-bold mb-2">Muhammad Wafa</p>
                 <p className="font-mono font-bold mb-2">Software Developer</p>
                 <p className="font-thin">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam libero ad ratione, nesciunt adipisci temporibus omnis, modi recusandae soluta molestias optio maxime magnam consequuntur odit, corrupti sunt eaque distinctio voluptatem!
