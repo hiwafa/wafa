ca1c21973734d5c95aa8a381cb37a0ac598c6a84
hiwafa <mq.func@gmail.com>
2024-04-18 14:04:18 +0200

    profile update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { MdMarkEmailRead } from "react-icons/md";
+import { MdMarkEmailRead, MdLocationPin } from "react-icons/md";
 import { FaPhoneSquareAlt } from "react-icons/fa";
+import { RiUserLocationFill } from "react-icons/ri";
 
 export default () => {
 
@@ -10,14 +11,19 @@ export default () => {
            <section className="float-left mr-3 shadow-md w-full flex flex-col items-center sm:w-72 min-w-72 rounded-md mb-5">
             <Image className="rounded-md self-center " src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
             <div className="mt-2">
-                <p className="font-serif font-bold mb-2">Muhammad Wafa</p>
+                <p className="font-mono font-bold mb-2 text-center">Muhammad Wafa</p>
                 <div className="w-full flex items-center gap-4 text-wrap">
                     <MdMarkEmailRead />
-                    <p className="font-mono font-bold mb-2 ">muhammadwfa@gmail.com</p>
+                    <p className="font-mono mb-2 text-sm">muhammadwfa@gmail.com</p>
                 </div>
                 <div className="w-full flex items-center gap-4">
                     <FaPhoneSquareAlt />
-                    <p className="font-mono font-bold mb-2">0178889999</p>
+                    <p className="font-mono mb-2 text-sm">0178889999</p>
+                </div>
+                <div className="w-full items-center mt-3">
+                    <RiUserLocationFill size={18} className="self-start float-left mr-4" />
+                    <p className="font-mono mb-2 text-sm ">Bauerngasse 38</p>
+                    <p className="font-mono mb-2 text-sm">94444 Schweinfurt</p>
                 </div>
             </div>
            </section>
