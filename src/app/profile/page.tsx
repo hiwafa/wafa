b16251451a70ff7f994103c368da1bf572595799
hiwafa <mq.func@gmail.com>
2024-04-19 15:31:21 +0200

    fixing overflowing

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,25 +9,24 @@ export default () => {
     return (
         <div className="m-5">
 
-           <section className="float-left mr-3 shadow-md w-full flex flex-col items-center sm:w-72 min-w-72 rounded-md mb-5">
-            <Image className="rounded-md self-center " src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
-            <div className="mt-5">
+           <section className="float-left mr-3 shadow-md w-full flex flex-col sm:w-72 min-w-72 rounded-md mb-5 pl-10">
+            <Image className="rounded-md self-center mr-10" src="/profileImg.jpeg" width={150} height={150} alt="Profile Image"/>
+            <div className="mt-5 space-y-2">
                 <p className="font-mono font-bold mb-2 uppercase">Kontact</p>
                 <div className="w-full flex items-center gap-4 text-wrap">
                     <MdMarkEmailRead />
-                    <p className="font-mono mb-2 text-sm">muhammadwfa@gmail.com</p>
+                    <p className="font-mono text-sm">muhammadwfa@gmail.com</p>
                 </div>
                 <div className="w-full flex items-center gap-4">
                     <FaPhoneSquareAlt />
-                    <p className="font-mono mb-2 text-sm">0178889999</p>
+                    <p className="font-mono text-sm">0178889999</p>
                 </div>
-                <div className="w-full items-center mt-3">
-                    <RiUserLocationFill size={18} className="self-start float-left mr-4" />
-                    <p className="font-mono mb-2 text-sm ">Bauerngasse 38</p>
-                    <p className="font-mono mb-2 text-sm">94444 Schweinfurt</p>
+                <div className="w-full flex">
+                    <RiUserLocationFill size={18} className="self-start mr-4" />
+                    <p className="font-mono text-sm ">Bauerngasse 38, 94444 Schweinfurt</p>
                 </div>
             </div>
-            <div className="mt-5 space-y-2 self-center">
+            <div className="mt-5 space-y-2">
                 <p className="font-mono font-bold mb-2 uppercase">Skills</p>
                 <div className="w-full flex items-center gap-4">
                     <FaJava />
@@ -55,7 +54,7 @@ export default () => {
            <div className="mt-5">
                 <div className="inline-flex sm:block">
                     <span className="w-1 bg-emerald-500 h-auto mr-2" />
-                    <div className=" sm:bg-green-100 rounded ">
+                    <div className=" sm:bg-green-100 rounded overflow-auto">
                         <p className="font-mono font-bold mb-2 text-xl break-break">Muhammad Wafa wafa wafa</p>
                         <p className="font-bold mb-2 text-emerald-500 break-break">Software Developer</p>
                     </div>
