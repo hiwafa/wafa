a8eaab568518ed0ba37dbed67b6c135a70a1ef62
hiwafa <mq.func@gmail.com>
2024-04-23 14:40:28 +0200

    added current active color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,12 @@ import { IoIosMenu } from "react-icons/io";
 import { useRef, useState } from "react";
 import { GiMoon } from "react-icons/gi";
 import Link from 'next/link';
+import { usePathname } from "next/navigation";
 
 export default () => {
 
   const mobileMenuRef = useRef(null);
+  const currentPath = usePathname();
 
   const handleMobileMenu = () => {
 
@@ -29,17 +31,17 @@ export default () => {
   }
 
   return (
-    <div className="bg-indigo-300 flex justify-between h-16 ">
+    <div className="flex justify-between h-16 shadow-md">
       <section className=" content-center md:ml-10 ml-5">
         <Image src="/logo.png" alt="logo" width={50} height={50} className="w-12 h-12 object-contain " />
       </section>
 
       <nav className={`hidden sm:flex gap-3 items-center`}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About Us</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/profile">Profile</Link>
+        <Link className={`${currentPath == '/' && 'border-emerald-500 border-b-2'}`} href="/">Home</Link>
+        <Link className={`${currentPath == '/about' && 'border-emerald-500 border-b-2'}`} href="/about">About Us</Link>
+        <Link className={`${currentPath == '/services' && 'border-emerald-500 border-b-2'}`} href="/services">Services</Link>
+        <Link className={`${currentPath == '/contact' && 'border-emerald-500 border-b-2'}`} href="/contact">Contact</Link>
+        <Link className={`${currentPath == '/profile' && 'border-emerald-500 border-b-2'}`} href="/profile">Profile</Link>
       </nav>
 
       <nav ref={mobileMenuRef} className={`absolute hidden bg-blue-500 w-full h-48 flex-col items-start justify-evenly text-white space-x-10`}>
