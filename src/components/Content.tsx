524684a882d09581d7d52b029451208ead2040aa
hiwafa <mq.func@gmail.com>
2024-04-23 15:16:12 +0200

    worked on a layout

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,19 @@
 
-export default ()=> {
+export default () => {
 
   return (
     <div className="bg-orange-300">
-      Main Content
+      <div className="w-96 h-72 border-2 border-emerald-200 m-10 resize overflow-auto flex flex-row gap-1 flex-wrap items-start content-start">
+
+
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+        <div className="w-16 h-16 bg-orange-200 flex-grow"></div>
+
+      </div>
     </div>
   );
 }
