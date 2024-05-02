1f6e1c7368b27f1522883fa81436107162ccf984
hiwafa <mq.func@gmail.com>
2024-05-02 15:44:55 +0200

    adding new files and layout updated

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -7,7 +7,7 @@ export default () => {
     <div className=" m-10 flex flex-row gap-1 flex-wrap items-start content-start">
 
 
-      <div className="w-96 h-72 flex flex-col flex-grow text-white font-mono overflow-y-auto">
+      <div className="w-96 min-h-72 flex flex-col flex-grow text-white font-mono">
 
         <p className="font-bold text-4xl">
           New language, new opportunities, new you
@@ -22,17 +22,19 @@ export default () => {
       </div>
 
 
-    
+
 
 
 
       <div className="w-56 min-h-48  flex-grow">
-      <Image src="/learneverywhere.png" width={300} height={200} alt="language show" className="w-auto h-auto " />
+        <Image src="/learneverywhere.png" width={300} height={200} alt="language show" className="w-auto h-auto " />
       </div>
       <div className="w-56 min-h-48 flex-grow">
         <Image src="/learnboy1.png" width={300} height={200} alt="language show" className="w-auto h-auto" />
       </div>
-      <div className="w-56 min-h-48 bg-orange-200 flex-grow"></div>
+      <div className="w-56 min-h-48 flex-grow">
+        <Image src="/withlaptop.png" width={300} height={200} alt="language show" className="w-auto h-auto" />
+      </div>
       <div className="w-56 min-h-48 bg-orange-200 flex-grow"></div>
       <div className="w-56 min-h-48 bg-orange-200 flex-grow"></div>
       <div className="w-56 min-h-48 bg-orange-200 flex-grow"></div>
